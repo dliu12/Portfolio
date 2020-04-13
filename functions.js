d999a297a20ac96a35b7be845b90112cb9e0d219
MyNameIs <liu@cooper.edu>
2020-04-13 12:54:03 -0400

    4/13/2020_Update
    Updated:
    1. Content of other projects - CAD
    2. Updated functions.js
    3. Updated Slide function in each page

--- functions.js
+++ functions.js
@@ -1,8 +1,12 @@
-let i = 0;
 function Slide(length,element,ImageArr){
-    i === length-1? i=0
-    :i++;
-    console.log(i);
-    element.style.backgroundImage = "url("+ImageArr[i]+")";
-    return i;
+    let i=0;
+    function SlideIn(){
+        i === length-1? i=0
+        :i++;
+        console.log(i);
+        element.style.backgroundImage = "url("+ImageArr[i]+")";
+        return i;
+    }
+    setInterval(function(){SlideIn()},3000);
+    return;
 };
